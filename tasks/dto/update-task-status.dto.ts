f5b518be4523dfdccaef703f2cdc2bf5799fc504
Vatsaljoshi1 <90548191+Vatsaljoshi1@users.noreply.github.com>
2022-06-02 12:13:17 +0530

    Add files via upload

---
+++ tasks/dto/update-task-status.dto.ts
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+import { IsEnum } from 'class-validator';
+import { TaskStatus } from '../task-status.enum';
+export class UpdateTaskStatusDto {
+  @IsEnum(TaskStatus)
+  status: TaskStatus;
+}
